45591720c782d360383bd2ef7eac29dd5a401343
Maria <msegber@gmail.com>
2024-11-21 17:54:59 +0100

    feat: create initial layout for bike configurator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Footer } from "./components/layout/Footer/Footer";
 import { Header } from "./components/layout/Header/Header";
+import { BikeConfigurator } from "./components/pages/BikeConfigurator/BikeConfigurator";
 
 function App() {
   return (
     <>
       <Header />
       <main className="appWrapper">
-        <h1>Markus Bike Shop</h1>
-        <div></div>
+        <BikeConfigurator />
       </main>
       <Footer />
     </>
---
+++ src/components/pages/BikeConfigurator/BikeConfigurator.tsx
@@ -0,0 +1,13 @@
+import "./BikeConfigurator.css";
+
+export const BikeConfigurator = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Dessing Your Bike</h1>
+      <div className="configurator">
+        <div className="section">{/* Section 1: Images */}</div>
+        <div className="section">{/* Section 2:  Design options*/}</div>
+      </div>
+    </div>
+  );
+};
