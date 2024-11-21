a4728b3d1965cc7f4695297352a53b9d463fc63c
Maria <msegber@gmail.com>
2024-11-21 16:53:19 +0100

    feat: create page header

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <button className="logo-button">
+        <img className="logo" src="./icons/logo-full.svg" />
+      </button>
+    </header>
+  );
+};
