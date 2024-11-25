bff7b585c1a099cbb09a7a7c1603f16fe71b2d15
Maria <msegber@gmail.com>
2024-11-25 10:47:07 +0100

    build: configure path alias for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": "/src/components",
+      "@data": "/src/data",
+      "@hooks": "/src/hooks",
+      "@layout": "/src/components/layout",
+      "@pages": "/src/components/pages",
+      "@ui": "/src/components/ui",
+    },
+  },
+});
