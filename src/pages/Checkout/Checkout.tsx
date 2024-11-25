790f9693fffc0e9b24219647e45cf32c550cb6dd
Maria <msegber@gmail.com>
2024-11-25 20:16:05 +0100

    feat: add checkout styles

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -7,20 +7,42 @@ import { Link } from "react-router-dom";
 export const Checkout = () => {
   const { bikeConfig, totalPrice } = useContext(BikeConfigContext);
 
-  console.log(bikeConfig);
-
   return (
     <div className={styles.container}>
-      <div className={styles.total}>
-        <h3>Total: </h3> <span>${totalPrice}</span>
+      <h1 className={styles.title}>Order Details</h1>
+      <div className={styles.bikeDetails}>
+        <div className={styles.details}>
+          <p className={styles.detailTitle}>Frame Type: </p>
+          <p className={styles.detailSubtitle}> {bikeConfig.frameType}</p>
+        </div>
+        <div className={styles.details}>
+          <p className={styles.detailTitle}>Frame Finish:</p>
+          <p className={styles.detailSubtitle}>{bikeConfig.frameFinish}</p>
+        </div>
+        <div className={styles.details}>
+          <p className={styles.detailTitle}>Wheel Type: </p>
+          <p className={styles.detailSubtitle}> {bikeConfig.wheelType}</p>
+        </div>
+        <div className={styles.details}>
+          <p className={styles.detailTitle}>Rim Color: </p>
+          <p className={styles.detailSubtitle}> {bikeConfig.rimColor}</p>
+        </div>
+        <div className={styles.details}>
+          <p className={styles.detailTitle}>Chain Type: </p>
+          <p className={styles.detailSubtitle}> {bikeConfig.chainType} </p>
+        </div>
+        <div className={styles.details}>
+          <p className={styles.detailTitle}>Total: </p>
+          <p className={styles.detailSubtitle}>${totalPrice}</p>
+        </div>
       </div>
-      <div className={styles.formActions}>
+
+      <div className={styles.actions}>
         <Link to="/bike-configurator" className={styles.link}>
           <Button type="reset" variant="secondary">
             Continue editing
           </Button>
         </Link>
-
         <Button onClick={() => console.log("PLace order")}>Submit</Button>
       </div>
     </div>
