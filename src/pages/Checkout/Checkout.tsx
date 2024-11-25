bce159615268b4661679599d50182b9ab52da8e6
Maria <msegber@gmail.com>
2024-11-25 21:29:07 +0100

    feat: add preview to the checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -11,36 +11,45 @@ export const Checkout = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>Order Details</h1>
       <div className={styles.bikeDetails}>
-        <div className={styles.details}>
-          <p className={styles.detailTitle}>Frame Type: </p>
-          <p className={styles.detailSubtitle}> {bikeConfig.frameType}</p>
+        <div className={styles.images}>
+          <img
+            src={`img/bikes/bike_${bikeConfig.frameFinish || "matte"}.png`}
+            alt={`Bike with ${bikeConfig.frameFinish || "matte"} finish`}
+            className={styles.bikeImage}
+          />
         </div>
-        <div className={styles.details}>
-          <p className={styles.detailTitle}>Frame Finish:</p>
-          <p className={styles.detailSubtitle}>{bikeConfig.frameFinish}</p>
-        </div>
-        <div className={styles.details}>
-          <p className={styles.detailTitle}>Wheel Type: </p>
-          <p className={styles.detailSubtitle}> {bikeConfig.wheelType}</p>
-        </div>
-        <div className={styles.details}>
-          <p className={styles.detailTitle}>Rim Color: </p>
-          <p className={styles.detailSubtitle}> {bikeConfig.rimColor}</p>
-        </div>
-        <div className={styles.details}>
-          <p className={styles.detailTitle}>Chain Type: </p>
-          <p className={styles.detailSubtitle}> {bikeConfig.chainType} </p>
-        </div>
-        <div className={styles.details}>
-          <p className={styles.detailTitle}>Total: </p>
-          <p className={styles.detailSubtitle}>${totalPrice}</p>
+        <div className={styles.summary}>
+          <div className={styles.details}>
+            <p className={styles.detailTitle}>Frame Type: </p>
+            <p className={styles.detailSubtitle}> {bikeConfig.frameType}</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailTitle}>Frame Finish:</p>
+            <p className={styles.detailSubtitle}>{bikeConfig.frameFinish}</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailTitle}>Wheel Type: </p>
+            <p className={styles.detailSubtitle}> {bikeConfig.wheelType}</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailTitle}>Rim Color: </p>
+            <p className={styles.detailSubtitle}> {bikeConfig.rimColor}</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailTitle}>Chain Type: </p>
+            <p className={styles.detailSubtitle}> {bikeConfig.chainType} </p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailTitle}>Total: </p>
+            <p className={styles.detailSubtitle}>${totalPrice}</p>
+          </div>
         </div>
       </div>
 
       <div className={styles.actions}>
         <Link to="/bike-configurator" className={styles.link}>
           <Button type="reset" variant="secondary">
-            Continue editing
+            Edit
           </Button>
         </Link>
         <Button onClick={() => console.log("PLace order")}>Submit</Button>
